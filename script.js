514e265c2aa379f12b26aa0d579e3d427ea9c8ab
bill6653 <billcrs512@gmail.com>
2022-09-23 13:18:42 -0500

    added random password generator with specifications

--- script.js
+++ script.js
@@ -10,32 +10,37 @@ function writePassword() {
 
 }
 
-function generatePassword (){
+function generatePassword() {
 
-var passwordLength =parseInt(prompt("Enter password Length?"));
-if(passwordLength<8 || passwordLength> 126 || isNaN(passwordLength)){
-  return "Invalid Password Length entered...Please enter valid characters"
-}
-var uppercaseYes = confirm("Do you like to have Uppercase in your password");
-var lowercaseYes = confirm("Must have a lower case letter in password");
-var lowercaseNumbers = confirm("Must have a lowercase number")
-var lowercaseSymbols = confirm("Must have Lowercase Symbols")
-var userChoice = []
-if(uppercaseYes){
-  userChoice += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-}
-if(lowercaseYes) {
-  userChoice += 'abcdefghijklmnopqrstuvwxyz'
-}
-if(lowercaseNumbers) {
-  userchoice += '0123456789'
-}
-if (lowercaseSymbols){
-  userChoice += '~!@#$%^&*()_+'
-}
-if (userChoice.length === 0){
-  return 'Invalid Password'
-}
+  var passwordLength = parseInt(prompt("Enter password Length?"));
+  if (passwordLength < 8 || passwordLength > 126 || isNaN(passwordLength)) {
+    return "Invalid Password Length entered...Please enter valid characters"
+  }
+  var uppercaseYes = confirm("Do you like to have Uppercase in your password");
+  var lowercaseYes = confirm("Must have a lower case letter in password");
+  var lowercaseNumbers = confirm("Must have a lowercase number")
+  var lowercaseSymbols = confirm("Must have Lowercase Symbols")
+  var userChoice = []
+  if (uppercaseYes) {
+    userChoice += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+  if (lowercaseYes) {
+    userChoice += 'abcdefghijklmnopqrstuvwxyz'
+  }
+  if (lowercaseNumbers) {
+    userChoice += '0123456789'
+  }
+  if (lowercaseSymbols) {
+    userChoice += '~!@#$%^&*()_+'
+  }
+  if (userChoice.length === 0) {
+    return 'Invalid Password character selection'
+  }
+  var passCode = ""
+  for(let i=0 ;i<passwordLength ;i++){
+    passCode += userChoice[Math.floor(Math.random() * userChoice.length)]
+  }
+  return passCode
 }
 
 
