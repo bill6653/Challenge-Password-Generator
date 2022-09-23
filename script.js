9d9ab9c7268d5bf1b675a80e1cffb70ae72580fc
bill6653 <billcrs512@gmail.com>
2022-09-23 13:10:18 -0500

    added specifications to password generator

--- script.js
+++ script.js
@@ -16,7 +16,26 @@ var passwordLength =parseInt(prompt("Enter password Length?"));
 if(passwordLength<8 || passwordLength> 126 || isNaN(passwordLength)){
   return "Invalid Password Length entered...Please enter valid characters"
 }
-
+var uppercaseYes = confirm("Do you like to have Uppercase in your password");
+var lowercaseYes = confirm("Must have a lower case letter in password");
+var lowercaseNumbers = confirm("Must have a lowercase number")
+var lowercaseSymbols = confirm("Must have Lowercase Symbols")
+var userChoice = []
+if(uppercaseYes){
+  userChoice += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+}
+if(lowercaseYes) {
+  userChoice += 'abcdefghijklmnopqrstuvwxyz'
+}
+if(lowercaseNumbers) {
+  userchoice += '0123456789'
+}
+if (lowercaseSymbols){
+  userChoice += '~!@#$%^&*()_+'
+}
+if (userChoice.length === 0){
+  return 'Invalid Password'
+}
 }
 
 
